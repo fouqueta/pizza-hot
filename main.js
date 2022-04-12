26a27c3989ff274400518d7a5c286d08fa58a194
fouqueta <aurfouquet@gmail.com>
2022-04-12 20:27:45 +0200

    correction serveur et renommage

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const serveur = express();
 const port = 8080;
 
 serveur.set('view engine', 'ejs');
+serveur.use(express.static('public'));
 
-serveur.get('/',function (req,res) {
-    res.sendFile("menu.html", {root:'public'});
+serveur.get('/', function (req,res) {
+    res.sendFile("accueil.html", {root:'public'});
 });
 
 // serv.get('/livraison',function (req,res) {
