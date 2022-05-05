d7ab0c0c664a450c944fda1203caff63943b26f4
Julia TAN <juliatan@hotmail.fr>
2022-05-05 21:21:54 +0200

    WIP: pool de connexion entre bd et js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const db = require('./queries');
 const serveur = express();
 const port = 8080;
+
+
 
 serveur.set('view engine', 'ejs');
 serveur.use(express.static('public'));
@@ -9,11 +12,34 @@ serveur.get('/', function (req,res) {
     res.sendFile("accueil.html", {root:'public'});
 });
 
-// serv.get('/livraison',function (req,res) {
-// });
+/*
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'init',
+    password: 'password',
+    port: 5432,
+})
 
+async function operations() {
+    const client = await pool.connect();
+// requêtes, traitement...
+    let res = await client.query ("SELECT * FROM pizza");
+// dernière étape indispensable, libérer le client :
+    client.release();
+// retour facultatif d'un résultat :
+    return resultat;
+}
+operations()
+    .then(resultat => { console.log(resultat)})
+    .catch(err => console.err (err.stack)); // si une erreur se produit.
+
+
+*/
 serveur.listen(8080,
     () => {
         console.log(`Server running at http://localhost:${port}/`);
     }
 );
+
+
