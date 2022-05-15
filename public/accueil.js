3df060a1afc76a4c60a70e871f23fed5a6a027a2
fouqueta <aurfouquet@gmail.com>
2022-05-15 22:00:12 +0200

    Autre méthode pour la barre de navigation + nouvelle couleur

--- public/accueil.js
+++ public/accueil.js
@@ -1,46 +1,57 @@
-$(document).ready(function(){
+
+
+$(document).ready(function() {
+    var div_menus = $('#div_produits_menus');
+    var div_entrees = $('#div_produits_entrees');
+    var div_pizzas = $('#div_produits_pizzas');
+    var div_boissons = $('#div_produits_boissons');
+    var div_desserts = $('#div_produits_desserts');
+    var div_sauces = $('#div_produits_sauces');
+
+    
+    div_entrees.hide();
+    div_pizzas.hide();
+    div_boissons.hide();
+    div_desserts.hide();
+    div_sauces.hide();
+    // init_produits();
+
     document.getElementById('carte_menus').onclick = function() {
-        console.log("oui");
-        replace('carte_menus');
-    }
-    document.getElementById('carte_entrees').onclick = function() {
-        console.log("oui");
-        replace('carte_entrees');
-    }
-    document.getElementById('carte_pizzas').onclick = function() {
-   // $("#carte_pizzas").click(function(){
-        console.log("oui");
-        replace('carte_pizzas');
-    }
-    document.getElementById('carte_boissons').onclick = function() {
-        console.log("oui");
-        replace('carte_boissons');
-    }
-    document.getElementById('carte_desserts').onclick = function() {
-        console.log("oui");
-        replace('carte_desserts');
-    }
-    document.getElementById('carte_sauces').onclick = function() {
-        console.log("oui");
-        replace('carte_sauces');
+        replace(div_menus);
     }
 
-    function replace(s){
-        document.getElementById('div_produits').innerHTML = '';
-        let newDivContent = document.createElement('div');
-        newDivContent.innerHTML = "cc je suis le nouveau div " + s;
-        document.getElementById('div_produits').appendChild(newDivContent);
+    document.getElementById('carte_entrees').onclick = function() {
+        replace(div_entrees);
+    }
+
+    document.getElementById('carte_pizzas').onclick = function() {
+        replace(div_pizzas);
+    }
+
+    document.getElementById('carte_boissons').onclick = function() {
+        replace(div_boissons);
+    }
+
+    document.getElementById('carte_desserts').onclick = function() {
+        replace(div_desserts);
+    }
+
+    document.getElementById('carte_sauces').onclick = function() {
+        replace(div_sauces);
+    }
+
+
+    function replace(s) {
+        $('#div_produits>div').hide();
+        s.show();
+        // let newDivContent = document.createElement('div');
+        // newDivContent.innerHTML = "cc je suis le nouveau div " + s;
+        // document.getElementById('div_produits').appendChild(newDivContent);
+    }
+
+    function init_produits() {
+
     }
 });
 
 
-
-
-
- /*
-(data)=>{
-    let doc = document.open('text/html','replace');
-    doc.write(data)
-    doc.close();
-}
-*/
