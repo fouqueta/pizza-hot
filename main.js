034fc1125264e70f776adadfb571524e0fe1d0ab
fouqueta <aurfouquet@gmail.com>
2022-05-17 18:17:28 +0200

    affichage non complet des items dans les catégories

--- main.js
+++ main.js
@@ -1,41 +1,93 @@
 const express = require('express');
-const pg = require('pg');
 const serveur = express();
+const pg = require('pg');
+
+const pool = new pg.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'pizzahot',
+    password: 'mettre_mdp',
+    port: 5432,
+});
+
+
+async function init_items() {
+    const client = await pool.connect();
+    let items = {
+        menus: await client.query ("SELECT * FROM menu"),
+        pizzas: await client.query ("SELECT nom, taille, prix FROM pizza"),
+        ingredients: await client.query ("SELECT * FROM ingredient"),
+        entrees: await client.query ("SELECT nom, prix, sauce FROM entree"),
+        boissons: await client.query ("SELECT nom, volume, prix FROM boisson"),
+        desserts: await client.query ("SELECT nom, prix FROM dessert"),
+        sauces: await client.query ("SELECT nom, prix FROM sauce"),
+    };
+    client.release();
+    return items;
+}
+
+// init_items(menus);
+
+// async function init_menus(tab) {
+//     const client = await pool.connect();
+//     let nb = await client.query ("SELECT count(nom) FROM menu");
+//     for (let i = 0; i < nb; i++) {
+//         tab[i] = {
+//             nom: await client.query ("SELECT nom FROM menu")
+
+//         }
+//     }
+//     let menus = await client.query ("SELECT * FROM menu");
+//     client.release();
+// }
+
+
+
+// async function init_items(items) {
+//     const client = await pool.connect();
+//     let menus = await client.query ("SELECT * FROM menu");
+//     let pizzas = await client.query ("SELECT nom, taille, prix FROM pizza");
+//     let ingredients = await client.query ("SELECT * FROM ingredient");
+//     let entrees = await client.query ("SELECT nom, prix, sauce FROM entree");
+//     let boissons = await client.query ("SELECT nom, volume, prix FROM boisson");
+//     let desserts = await client.query ("SELECT nom, prix FROM dessert");
+//     let sauces = await client.query ("SELECT nom, prix FROM sauce");
+//     client.release();
+//     return [menus, pizzas, ingredients, entrees, boissons, desserts, sauces];
+// }
+
+var items = {};
+
+init_items()
+    .then(resultat => { items = resultat })
+    .catch(err => console.err (err.stack));
+
 
 serveur.set('view engine', 'ejs');
+serveur.use(express.urlencoded({extended: true}));
 serveur.use(express.static('public'));
 
+
+// serveur.get('/', function (req,res) {
+//     res.render('accueil.ejs', {items});
+// });
+
 serveur.get('/', function (req,res) {
-    res.sendFile("accueil.html", {root:'public'});
+    res.render('accueil.ejs', {menus: items.menus.rows, 
+        pizzas: items.pizzas.rows,
+        ingredients: items.ingredients.rows,
+        entrees: items.entrees.rows,
+        boissons: items.boissons.rows,
+        desserts: items.desserts.rows,
+        sauces: items.sauces.rows});
 });
 
 // serv.get('/livraison',function (req,res) {
 // });
 
-const pool = new pg.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'pizzaHot',
-    password: 'mettre_mdp',
-    port: 5432,
-});
-
-async function operations() {
-    const client = await pool.connect();
-// requêtes, traitement...
-    let res = await client.query ("SELECT * FROM pizza");
-// dernière étape indispensable, libérer le client :
-    client.release();
-// retour facultatif d'un résultat :
-    return res;
-}
-operations()
-    .then(resultat => { console.log(resultat)})
-    .catch(err => console.err (err.stack)); // si une erreur se produit.
-
 
 serveur.listen(8080,
     () => {
-        console.log(`Server running at http://localhost:8080}/`);
+        console.log(`Server running at http://localhost:8080`);
     }
 );
---
+++ public/accueil.js
@@ -0,0 +1,67 @@
+ 
+$(document).ready(function() {
+    var div_menus = $('#div_produits_menus');
+    var div_entrees = $('#div_produits_entrees');
+    var div_pizzas = $('#div_produits_pizzas');
+    var div_boissons = $('#div_produits_boissons');
+    var div_desserts = $('#div_produits_desserts');
+    var div_sauces = $('#div_produits_sauces');
+
+    div_entrees.hide();
+    div_pizzas.hide();
+    div_boissons.hide();
+    div_desserts.hide();
+    div_sauces.hide();
+
+    // init_produits(div_menus);
+
+    document.getElementById('carte_menus').onclick = function() {
+        replace(div_menus);
+    }
+
+    document.getElementById('carte_entrees').onclick = function() {
+        replace(div_entrees);
+    }
+
+    document.getElementById('carte_pizzas').onclick = function() {
+        replace(div_pizzas);
+    }
+
+    document.getElementById('carte_boissons').onclick = function() {
+        replace(div_boissons);
+    }
+
+    document.getElementById('carte_desserts').onclick = function() {
+        replace(div_desserts);
+    }
+
+    document.getElementById('carte_sauces').onclick = function() {
+        replace(div_sauces);
+    }
+
+
+    function replace(s) {
+        $('#div_produits>div').hide();
+        s.show();
+    }
+
+
+    // function init_produits(s) {
+    //     // let div_i = '<img src="/images/coincoin.jpg" alt="coincoin" class="img-fluid" alt="Responsive image"/>';
+    //     // let x = $(div_i);
+    //     // div_menus.append(x);
+    //     for (let i = 0; i < 15; i++) {
+    //         let div_item = '<div';
+    //         div_item += ' id="item_' + s + '_' + i + '"';
+    //         div_item += ' class="div_produit col-12 col-sm-6 col-md-6 col-lg-4 text-center">';
+    //         div_item += '<div class="inside_div_produit"><img src="images/coincoin.jpg" alt="coincoin" class="img-fluid" alt="Responsive image"/>';
+    //         div_item += '<p>Test</p></div></div>';
+    //         //div_item += '<img src=images/' + + i + '.jpg /></div>';
+    //         let el = $(div_item);
+    //         s.append(el);
+    //     }
+    // }
+
+});
+
+
